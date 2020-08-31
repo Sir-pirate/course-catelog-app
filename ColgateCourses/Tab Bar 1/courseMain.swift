cb5a222063578bfaed18ad0c55deb948eaf09d24
Sir-pirate <48810899+Sir-pirate@users.noreply.github.com>
2020-08-30 23:45:04 -0400

    Update courseMain.swift

--- ColgateCourses/Tab Bar 1/courseMain.swift
+++ ColgateCourses/Tab Bar 1/courseMain.swift
@@ -19,20 +19,18 @@ class courseMain: UITableViewController {
         self.tableView.separatorStyle = .none
         if let DCsubject = specificSubject{
             navigationItem.title  = DCsubject.name.rawValue
-            
-        }
-        
-        
+           //set navigation title to be the subject name (e.g. Computer Science)
 
+        }
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+        // Number of Section
         return 1
     }
-
+   //
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+        // number of Rows in each sections
         if let DCsubject = specificSubject{
             if let DCcourseCollection = DCsubject.courseCollection{
                 return DCcourseCollection.count
@@ -47,15 +45,13 @@ class courseMain: UITableViewController {
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       //configure individual cell to display individual courses,(course name, favorite, comment, instructor name, course status)
         let cell = tableView.dequeueReusableCell(withIdentifier: "courseCell", for: indexPath) as! courseCell
-         
-
-        
-        cell.motherDelegate = self
+    
+        cell.motherDelegate = self//set delegate
        if let DCsubject = specificSubject{
            if let DCcourseCollection = DCsubject.courseCollection{
           
-            
             if let DCinstructor = DCcourseCollection[indexPath.row].INSTRUCTOR1_NAME{//set instructor name
                 cell.courseInstructor.text = DCinstructor}
             else{cell.courseInstructor.text = "TBD"}
@@ -76,25 +72,29 @@ class courseMain: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+       //header format
         return 5
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {// to courseDetailViewController
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+       // to courseDetailViewController, when user click on a specific course-->
+       //gather data before performing as well as passing data into next view control
         if segue.identifier == "courseToDetail",
             let  courseDetail = segue.destination as? courseDetalViewController{
-               
                 if let DCsubject = specificSubject{
                     if let DCcourseCollection = DCsubject.courseCollection{
                          let selectedIndexPath = tableView.indexPathForSelectedRow!
                          let selectedCourse = DCcourseCollection[selectedIndexPath.row]
                         courseDetail.CRN = selectedCourse.CRN
-                        
+          
                     }
                 }
-           
         }
         
-        if segue.identifier == "toComment", let commentMain = segue.destination as? Comment_Main{// to comment_main
+        if segue.identifier == "toComment", let commentMain = segue.destination as? Comment_Main{
+           // to comment_main, when user click on comment button 
+           //--> perform segue
+           //--> go to comment section
             if let DCcommentOfCourse = commentForCourse{
             commentMain.commentOfCourse = DCcommentOfCourse
             }
@@ -104,10 +104,13 @@ class courseMain: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // perform segue when clicked on specific course
         performSegue(withIdentifier: "courseToDetail", sender: nil)
     }
     
-    func comment(commentTapped:courseCell){// pass the clase object of the selected comment into comment_main
+    func comment(commentTapped:courseCell){
+       //User action (comment)
+       // pass the clase object of the selected comment into comment_main
         let indexpathClicked = tableView.indexPath(for: commentTapped)
         commentForCourse = specificSubject?.courseCollection![indexpathClicked!.row]
         
@@ -115,6 +118,7 @@ class courseMain: UITableViewController {
     
     
     func favoriteACell(favoriteCell:courseCell){
+       //User action(mark a course favorite)
         let indexpathClicked = tableView.indexPath(for: favoriteCell)
         let secondTab = (self.tabBarController?.viewControllers![1])! as! Favorite as Favorite
         secondTab.favoriteCollection.append((specificSubject?.courseCollection![indexpathClicked!.row])!)
@@ -123,7 +127,9 @@ class courseMain: UITableViewController {
           
         
      }
+   
     func unfavoriteACell(unfavoriteCell:courseCell){
+       //User action(unmark a course  favorite)
         let indexpathClicked = tableView.indexPath(for: unfavoriteCell)
         let secondTab = (self.tabBarController?.viewControllers![1])! as! Favorite as Favorite
         for i in secondTab.favoriteCollection{
